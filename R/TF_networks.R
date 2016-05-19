584a5f07af90a8249902911bb971dfb38a93c001
Shirin Glander <s_glan02@wwu.de>
2016-05-19 17:25:49 +0200

    Aded read_bam_to_countmatrix()

--- R/TF_networks.R
+++ R/TF_networks.R
@@ -8,15 +8,15 @@
 #'
 #' @param expmatrix An input matrix with gene names as row names and sample names as column names.
 #' @param nodeAnno A data frame with node annotation, e.g. logFC and differential expression (e.g. limma output). Rownames have to correspond to rownames of expmatrix!
-#' @param adjPcol Column name for adjusted p-values (used for creating a column "DE" in the output node annotation).
-#' @param p.value Significance threshold. Defaults to 0.05.
+#' @param GeneName Column name with Gene Symbol to be annotated to TF name.
 #' @param projectfolder File path where to save the output to. Defaults to working directory. Here, it saves the output to a subfolder called "Networks".
+#' @param outPrefix Prefix added to output name.
 #' @return A .expression output matrix of gene expression of transkription factors in dataset used as Biolayout3D input.
 #' And based on these TFs, it also produces a .txt with node annotations of e.g. logFC and differential expression (e.g. limma output)
 #' @examples
-#' TF_networks(expmatrix, nodeAnno=Allgenes_limma_pw)
+#' TF_networks(expmatrix, nodeAnno=Allgenes_limma_pw, outPrefix="Test")
 #' @export
-TF_networks <- function(expmatrix, nodeAnno, adjPcol="adj.P.Val", p.value=0.05, projectfolder = getwd()){
+TF_networks <- function(expmatrix, nodeAnno, GeneName = "row.names", projectfolder = getwd(), outPrefix){
 
   if (!file.exists(file.path(projectfolder, "Networks"))) {dir.create(file.path(projectfolder, "Networks")) }
 
@@ -24,15 +24,14 @@ TF_networks <- function(expmatrix, nodeAnno, adjPcol="adj.P.Val", p.value=0.05, 
 
   TF_gene_exprs <- merge(TFs_human,expmatrix, by.x = "Human", by.y = "row.names", all = FALSE)
 
-  utils::write.table(TF_gene_exprs, file.path(projectfolder, "Networks", "TF_gene_exprs.expression"), col.names = T, row.names = F, sep = "\t")
-  cat("\n-------------------------\n", "TF expression matrix saved to", file.path(projectfolder, "Networks", "TF_gene_exprs.expression"), "\n-------------------------\n")
+  utils::write.table(TF_gene_exprs, file.path(projectfolder, "Networks", paste0(outPrefix, "_TF_exprs.expression")), col.names = T, row.names = F, sep = "\t")
+  cat("\n-------------------------\n", "TF expression matrix saved to", file.path(projectfolder, "Networks", paste0(outPrefix, "_TF_exprs.expression")), "\n-------------------------\n")
 
 
-  DEunfiltered <- merge(TF_gene_exprs[,1:2], nodeAnno, by.x = "Human", by.y = "row.names", all = TRUE)
-  DEunfiltered$DE <- ifelse(DEunfiltered[,adjPcol] < p.value, "1", "0")
+  DEunfiltered <- merge(TF_gene_exprs[,1:2], nodeAnno, by.x = "Human", by.y = GeneName, all.x = TRUE, all.y = FALSE)
 
-  utils::write.table(DEunfiltered, file.path(projectfolder, "Networks", "TF_nodeAnno.txt"), col.names = T, row.names = F, sep = "\t", quote = F)
-  cat("\n-------------------------\n", "TF network node annotation saved to", file.path(projectfolder, "Networks", "TF_nodeAnno.txt"), "\n-------------------------\n")
+  utils::write.table(DEunfiltered, file.path(projectfolder, "Networks", paste0(outPrefix, "_TF_nodeAnno.txt")), col.names = T, row.names = F, sep = "\t", quote = F)
+  cat("\n-------------------------\n", "TF network node annotation saved to", file.path(projectfolder, "Networks", paste0(outPrefix, "_TF_nodeAnno.txt")), "\n-------------------------\n")
 
 }
 
---
+++ R/read_bam_to_countmatrix.R
@@ -0,0 +1,58 @@
+#' read_bam_to_countmatrix
+#'
+#' Reads in bam files and creates a count matrix based on a gtf file.
+#'
+#' @details So far, it is only implemented for obtaining read counts for genes based on summarized counts of reads overlapping all exons of each gene.
+#'
+#' @param sampleTable Sample Table. See Vignette for instructions on how to build this data frame.
+#' @param gtffile GTF file.
+#' @param projectfolder File path where to save the output to. Defaults to working directory. Here, it saves the output to a subfolder called "Networks".
+#' @param outPrefix Prefix added to output name.
+#' @param singleEnd A logical indicating if reads are single or paired-end. In Bioconductor > 2.12 it is not necessary to sort paired-end BAM files by qname. When counting with summarizeOverlaps, setting singleEnd=FALSE will trigger paired-end reading and counting. It is fine to also set asMates=TRUE in the BamFile but is not necessary when singleEnd=FALSE.
+#' @param ignore.strand A logical indicating if strand should be considered when matching.
+#' @param fragments A logical; applied to paired-end data only. fragments controls which function is used to read the data which subsequently affects which records are included in counting.
+#' When fragments=FALSE, data are read with readGAlignmentPairs and returned in a GAlignmentPairs class. This class only holds 'mated pairs' from opposite strands; same-strand pairs singletons, reads with unmapped pairs and other fragments are dropped.
+#' When fragments=TRUE, data are read with readGAlignmentsList and returned in a GAlignmentsList class. This class holds 'mated pairs' as well as same-strand pairs, singletons, reads with unmapped pairs and other fragments. Because more records are kept, generally counts will be higher when fragments=TRUE.
+#' The term 'mated pairs' refers to records paired with the algorithm described on the ?readGAlignmentsList man page.
+#' @return Writes the count matrix as .txt to file and returns a DESeq data set, which can then be used further with DESeq2.
+#' @examples
+#' read_bam_to_countmatrix(sampleTable, gtffile = "/media/data/Public/ENSEMBL.rel.83/Homo_sapiens.GRCh38.83.gtf", projectfolder = getwd(), outPrefix="Test")
+#' @export
+read_bam_to_countmatrix <- function(sampleTable, gtffile, projectfolder = getwd(), outPrefix, singleEnd=FALSE, ignore.strand=TRUE, fragments=TRUE){
+
+  filenames <- file.path(dir, paste0(sampleTable$filenames))
+
+  if(any(!file.exists(filenames))) stop("Bam file path does not exist, check spelling!")
+
+  bamfiles <- Rsamtools::BamFileList(filenames, yieldSize=2000000)
+
+  # Defining gene models
+    txdb <- GenomicFeatures::makeTxDbFromGFF(gtffile, format="gtf", circ_seqs=character())
+
+    # The following line produces a GRangesList of all the exons grouped by gene. Each element of the list is a GRanges object of the exons for a gene.
+    ebg <- exonsBy(txdb, by="gene")
+
+    ## Read counting step
+    BiocParallel::register(BiocParallel::SerialParam())
+
+    #The following call creates the SummarizedExperiment object with counts:
+
+    se <- GenomicAlignments::summarizeOverlaps(features=ebg, reads=bamfiles,
+                            mode="Union",
+                            singleEnd=singleEnd,
+                            ignore.strand=ignore.strand,
+                            fragments=fragments)
+
+    colData(se) <- DataFrame(sampleTable)
+
+    data <- DESeq2::DESeqDataSet(se, design = ~colData)
+
+    if (!file.exists(file.path(projectfolder, "Countdata"))) {dir.create(file.path(projectfolder, "Countdata")) }
+
+    utils::write.table(SummarizedExperiment::assay(data), file.path(projectfolder, "Countdata", paste0(outPrefix, "_Summarize_overlaps_count_matrix.txt")), col.names = T, row.names = F, sep = "\t")
+    cat("\n-------------------------\n", "Count matrix saved to", file.path(projectfolder, "Countdata", paste0(outPrefix, "_Summarize_overlaps_count_matrix.txt")), "\n-------------------------\n")
+
+    return(data)
+}
+
+# devtools::document()
